2f951ce83ed57a6277e033154d3e82db714e63c4
cadeauxramon603 <cadeauxramon@gmail.com>
2019-02-23 22:13:34 -0500

    youtube integration and multiple videos get to play

--- app/src/main/java/com/ramon/videoapp/moviedetails/FeaturedVideosViewHolder.java
+++ app/src/main/java/com/ramon/videoapp/moviedetails/FeaturedVideosViewHolder.java
@@ -1,11 +1,13 @@
 package com.ramon.videoapp.moviedetails;
 
+import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.ramon.videoapp.R;
@@ -19,8 +21,8 @@ import butterknife.OnClick;
 public class FeaturedVideosViewHolder extends RecyclerView.ViewHolder {
 
     private final YoutubeItemClicked clickListener;
-    @BindView(R.id.now_playing)
-    TextView nowPlayingText;
+    @BindView(R.id.related_click_area)
+    LinearLayout clickArea;
     @BindView(R.id.youtube_title)
     TextView youtubeTitle;
     @BindView(R.id.thumbnail_preview)
@@ -30,18 +32,19 @@ public class FeaturedVideosViewHolder extends RecyclerView.ViewHolder {
     public FeaturedVideosViewHolder(@NonNull View itemView, YoutubeItemClicked clickListener) {
         super(itemView);
         ButterKnife.bind(this, itemView);
-        this.clickListener=clickListener;
+        this.clickListener = clickListener;
     }
 
-    static FeaturedVideosViewHolder inflate(ViewGroup viewGroup,YoutubeItemClicked clickListener) {
+    static FeaturedVideosViewHolder inflate(ViewGroup viewGroup, YoutubeItemClicked clickListener) {
         View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.more_videos_vh, viewGroup, false);
-        return new FeaturedVideosViewHolder(view,clickListener);
+        return new FeaturedVideosViewHolder(view, clickListener);
     }
 
     void bind(@NonNull YoutubeItem item, boolean nowPlaying) {
-        this.youtubeItem=item;
+        this.youtubeItem = item;
+        clickArea.setBackgroundColor(nowPlaying ? itemView.getResources().getColor(R.color.colorPrimary, itemView.getResources().newTheme()) : Color.WHITE);
         youtubeTitle.setText(item.getSnippet().getTitle());
-        nowPlayingText.setVisibility(nowPlaying ? View.VISIBLE : View.GONE);
+        youtubeTitle.setTextColor(nowPlaying ? Color.WHITE : Color.BLACK);
         Picasso.get()
                 .load(item.getSnippet().getThumbnails().getDefault().getUrl())
                 .placeholder(R.drawable.ic_mood_black_24dp)
@@ -51,7 +54,7 @@ public class FeaturedVideosViewHolder extends RecyclerView.ViewHolder {
     }
 
     @OnClick(R.id.related_click_area)
-    public void clickArea(){
+    public void clickArea() {
         clickListener.itemClicked(youtubeItem);
     }
 }
